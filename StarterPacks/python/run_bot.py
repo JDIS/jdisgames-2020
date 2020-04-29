e5412747740369a5321596fbe4380203843010b5
Étienne Lévesque <etienne.levesque16@gmail.com>
2020-04-29 16:46:16 -0400

    Remove useless option in run_bot.py arg parsing

--- StarterPacks/python/run_bot.py
+++ StarterPacks/python/run_bot.py
@@ -89,9 +89,9 @@ def loop(secret, is_ranked):
 def str2bool(v):
     if isinstance(v, bool):
         return v
-    if v.lower() in ('yes', 'true', 't', 'y', '1', 'True'):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0', 'False'):
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
         return False
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
