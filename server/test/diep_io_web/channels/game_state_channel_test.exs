f92cd5b4ec825e386184c7e86e143c2469327ed5
Étienne Lévesque <etienne.levesque16@gmail.com>
2022-06-02 21:48:00 -0400

    refactor: Use different pubsub topics for different games (#22)
    * refactor: Use different pubsub topics for different games
    
    * test: Fix tests
    
    * chore: Fix credo

--- server/test/diep_io_web/channels/game_state_channel_test.exs
+++ server/test/diep_io_web/channels/game_state_channel_test.exs
@@ -20,26 +20,12 @@ defmodule DiepIOWeb.GameStateChannelTest do
 
   test "new_state broadcasts to game_state", %{game_name: game_name} do
     state = GameState.new(game_name, [], 1, false, false, Clock.new(1, 1))
-    PubSub.broadcast!("new_state", {:new_state, state})
+    PubSub.broadcast!("new_state:#{game_name}", {:new_state, state})
     assert_broadcast("new_state", ^state)
   end
 
   test "broadcasts are pushed to the client", %{socket: socket} do
     broadcast_from!(socket, "broadcast", %{"some" => "data"})
     assert_push("broadcast", %{"some" => "data"})
-  end
-
-  test "join/3 assigns game name as string to the socket", %{socket: socket, game_name: game_name} do
-    assert socket.assigns[:game_name] == to_string(game_name)
-  end
-
-  test "broadcasts are only pushed to clients who subscribed to the corresponding game", %{
-    game_name: game_name
-  } do
-    game_name = String.to_atom(to_string(game_name) <> "2")
-    state = GameState.new(game_name, [], 1, false, false, Clock.new(1, 1))
-    PubSub.broadcast!("new_state", {:new_state, state})
-
-    refute_push("new_state", ^state)
   end
 end
