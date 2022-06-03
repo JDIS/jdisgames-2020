dcab41a56cd5f3fa28020a3a1af4270dac19493d
ZeLarpMaster <guillaumejean46@gmail.com>
2022-06-02 20:32:12 -0400

    fix: Add missing env vars

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -5,9 +5,9 @@ sleep 5
 
 echo "Migrating database before starting..."
 
-SECRET_KEY_BASE=$(cat secret_key.txt) bin/diep_io eval "Diep.Io.Release.migrate"
+DIEP_SECRET_KEY_BASE=$(cat secret_key.txt) bin/diep_io eval "Diep.Io.Release.migrate"
 
 echo "Seeding database before starting..."
 
 echo "Starting JDISGames 2020 Server...."
-SECRET_KEY_BASE=$(cat secret_key.txt) bin/diep_io start
+DIEP_SECRET_KEY_BASE=$(cat secret_key.txt) bin/diep_io start
