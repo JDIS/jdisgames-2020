25646a6d7000c6ccbbccedf0a2ae107e142d84af
Francis Hamel <francishamel96@gmail.com>
2022-06-04 21:07:40 -0400

    fixup! refactor: Remove gettext

--- server/lib/diep_io_web/views/error_helpers.ex
+++ server/lib/diep_io_web/views/error_helpers.ex
@@ -9,8 +9,8 @@ defmodule DiepIOWeb.ErrorHelpers do
   Generates tag for inlined form input errors.
   """
   def error_tag(form, field) do
-    Enum.map(Keyword.get_values(form.errors, field), fn error ->
-      content_tag(:span, error, class: "help-block")
+    Enum.map(Keyword.get_values(form.errors, field), fn {error_msg, _opts} ->
+      content_tag(:span, error_msg, class: "help-block")
     end)
   end
 end
