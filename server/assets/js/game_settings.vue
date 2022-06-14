a00b9b6ed76cf0b1dbece262533148faacca04bc
ZeLarpMaster <guillaumejean46@gmail.com>
2022-06-13 22:18:48 -0400

    refactor: Better usage of search params

--- server/assets/js/game_settings.vue
+++ server/assets/js/game_settings.vue
@@ -3,16 +3,16 @@
     <div class="game-settings">
       <p class="game-settings-title">{{ game_title }}</p>
       <div class="label-input-wrapper">
-        <label class="game-time-label" for="main-game-ticks">Number of ticks:</label>
-        <input id="main-game-ticks" class="game-time-input" type="number" v-model="ticks" min="1" />
+        <label for="main-game-ticks">Number of ticks:</label>
+        <input id="main-game-ticks" type="number" v-model="ticks" min="1" />
       </div>
       <div class="label-input-wrapper">
-        <label class="game-debris-count-label" for="main-game-debris-count">Max debris count:</label>
-        <input id="main-game-debris-count" class="game-debris-count-input" type="number" v-model="debris_count" min="1" />
+        <label for="main-game-debris-count">Max debris count:</label>
+        <input id="main-game-debris-count" type="number" v-model="debris_count" min="1" />
       </div>
       <div class="label-input-wrapper">
-        <label class="game-debris-rate-label" for="main-game-debris-rate">Debris generation rate:</label>
-        <input id="main-game-debris-rate" class="game-debris-rate-input" type="number" v-model="debris_generation_rate" min="0" max="1" step="0.01" />
+        <label for="main-game-debris-rate">Debris generation rate:</label>
+        <input id="main-game-debris-rate" type="number" v-model="debris_generation_rate" min="0" max="1" step="0.01" />
       </div>
       <button @click="startGame()">Start</button>
       <button @click="stopGame()">Stop</button>
@@ -37,7 +37,14 @@ export default {
   mounted() {},
   methods: {
     startGame() {
-      window.location = `/admin/start?ticks=${this.ticks}&max_debris_count=${this.debris_count}&max_debris_generation_rate=${this.debris_generation_rate}&game_name=${this.$props.game_name}`;
+      const params = {
+        ticks: this.ticks,
+        max_debris_count: this.debris_count,
+        max_debris_generation_rate: this.debris_generation_rate,
+        game_name: this.$props.game_name,
+      };
+      const searchParams = new URLSearchParams(params);
+      window.location = `/admin/start?${searchParams.toString()}`;
     },
 
     stopGame() {
