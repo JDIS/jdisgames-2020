704699b8f29929a8945e49b221f9ca2933104e52
Émilio Gonzalez <egg997@gmail.com>
2022-07-22 13:57:12 -0400

    - Ajout de stock manquant dans la doc - Balançage du jeu

---
+++ server/priv/repo/migrations/20220722154854_edit_upgrade_params_2.exs
@@ -0,0 +1,20 @@
+defmodule DiepIO.Repo.Migrations.EditUpgradeParams2 do
+  use Ecto.Migration
+
+  def change do
+    alter table(:game_params) do
+      modify(:upgrade_params, :map,
+        null: false,
+        default: %{
+          speed: %{upgrade_rate: 0.3, base_value: 10},
+          max_hp: %{upgrade_rate: 0.3, base_value: 50},
+          projectile_damage: %{upgrade_rate: 0.3, base_value: 20},
+          body_damage: %{upgrade_rate: 0.3, base_value: 20},
+          fire_rate: %{upgrade_rate: 0.20, base_value: 25},
+          hp_regen: %{upgrade_rate: 0.3, base_value: 0.3},
+          projectile_time_to_live: %{upgrade_rate: 0.15, base_value: 30}
+        }
+      )
+    end
+  end
+end
